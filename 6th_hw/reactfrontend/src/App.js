b2c65a0b7a9a928a45cf07b67cd9ed18fb86d799
ningpop <craft1933@naver.com>
2019-12-31 01:17:03 +0900

    title 수정

--- 6th_hw/reactfrontend/src/App.js
+++ 6th_hw/reactfrontend/src/App.js
@@ -60,7 +60,7 @@ class App extends React.Component {
         <Container maxWidth="lg">
           <div className="PostingSection">
             <Paper className="PostingPaper">
-              <h2>대나무 숲 글 작성하기</h2>
+              <h2>LikeLion 7th 대나무 숲 글 작성하기 - @ningpop</h2>
               <form className="PostingForm" onSubmit={this.handlingSubmit}>
                 <TextField
                   id="outlined-name"
